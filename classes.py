9e61ece22b5a8f0b0e1dd7039088084b62d053e2
kimrugi-LapTop <kimrugi@naver.com>
2018-10-22 18:48:05 +0900

    클래스들 정리

---
+++ classes.py
@@ -0,0 +1,34 @@
+from pico2d import *
+
+images = (load_image('resources\\character\\shipsprite1.png'))
+
+
+class Object:
+    def __init__(self):
+        pass
+    pass
+
+
+class Player:
+    def __init__(self):
+        pass
+    pass
+
+
+class Enemy:
+    def __init__(self):
+        pass
+    pass
+
+
+class Bullet:
+    def __init__(self):
+        pass
+    pass
+
+
+
+
+
+
+
