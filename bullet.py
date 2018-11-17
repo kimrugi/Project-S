f63c18263d7e02c97e5b86549d0029e096469317
kimrugi-LapTop <kimrugi@naver.com>
2018-11-17 14:06:37 +0900

    bullet 방향 패치

--- bullet.py
+++ bullet.py
@@ -1,20 +1,22 @@
 from pico2d import *
 import framework
-from game_value import middle
+import math
 
 IMAGE_SIZE = (491, 344)
 BULLET_POSITION = -(int(190 - IMAGE_SIZE[1]))
 class Bullet:
     image = None
-    def __init__(self, weapon):
-        self.x = weapon.player.x
-        self.y = weapon.player.y
+
+    def __init__(self, x, y, vertical, horizon):
+        self.x = x
+        self.y = y
         self.size = 10
         if Bullet.image == None:
             Bullet.image = load_image('resources\\bullet\\bullet1.png')
         self.speed = 200
-        self.vertical = weapon.vertical
-        self.horizon = weapon.horizon
+        self.dir = math.atan2(vertical, horizon)
+        self.vertical = vertical
+        self.horizon = horizon
         pass
 
     def draw(self, screen):
@@ -22,7 +24,7 @@ class Bullet:
         pass
 
     def update(self):
-        self.x += self.speed * self.horizon * framework.frame_time
-        self.y += self.speed * self.vertical * framework.frame_time
+        self.x += self.speed * math.cos(self.dir) * framework.frame_time
+        self.y += self.speed * math.sin(self.dir) * framework.frame_time
         pass
     pass
