9e737163fc4d1e86beb94a8dde8068d13a7898bc
kimrugi-LapTop <kimrugi@naver.com>
2018-11-20 12:17:09 +0900

    행동트리 ai  findplayer 만듬

--- default_enemy.py
+++ default_enemy.py
@@ -1,5 +1,7 @@
 from pico2d import *
 import math
+import main_game
+from BehaviorTree import BehaviorTree, SelectorNode, SequenceNode, LeafNode
 
 PIXEL_PER_KILOMETER = 5
 QUAD_PI = math.pi / 4
@@ -20,16 +22,36 @@ class DefaultEnemy:
         self.horizon = 0
         self.calcul_speed(self.kmps)
         self.dir = math.pi
+        self.detect_range = 500
         pass
+
     def calcul_speed(self, kmps):
         self.kmps = kmps
         self.speed = self.kmps * PIXEL_PER_KILOMETER
     pass
 
+    def find_player(self):
+        player = main_game.get_player()
+        distance = (player.x - self.x) ** 2 + (player.y - self.y) ** 2
+        if distance < self.detect_range ** 2:
+            self.dir = rounds_pi(math.atan2(player.x - self.x, player.y - self.y))
+            return BehaviorTree.SUCCESS
+        else:
+            return BehaviorTree.FAIL
 
-
+    
 
 
 def rounds_pi(theta):
     result = theta + HALF_OF_QUAD_PI % QUAD_PI
     return result
+
+
+
+
+
+
+
+
+
+
